22a259fb5fcdf2f6a819581f2f5c0be039755e5b
nildiert <niljordan23@gmail.com>
2020-09-09 17:59:54 -0500

    [FEATURE] Added ListView

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ app_name = 'blog'
 urlpatterns = [
     # post vies
     path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
 ]
