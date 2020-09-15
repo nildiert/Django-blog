0b7f81de3000bbc555a525611a4b220b3ab47ca6
nildiert <niljordan23@gmail.com>
2020-09-15 00:43:25 -0500

    [FEATURE] Added taggit in order to manage the tags in my blog

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 # Create your models here.
 
 
@@ -25,6 +26,7 @@ class Post(models.Model):
     created = models.DateField(auto_now_add=True)
     updated = models.DateField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    tags = TaggableManager()
 
     def get_absolute_url(self):
         return reverse("blog:post_detail", args=[self.publish.year,
