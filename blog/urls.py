0c23450d72422f60740922586df2c922967aee1d
nildiert <niljordan23@gmail.com>
2020-09-15 14:56:05 -0500

    [FEATURE] Added RSS susbcription

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .feeds import LatestPostsFeed
 from . import views
 app_name = 'blog'
 urlpatterns = [
@@ -8,4 +9,5 @@ urlpatterns = [
     # path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
