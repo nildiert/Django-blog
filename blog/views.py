1fb797ca6e8addf31ea1f14c201dd3e6a92822ca
nildiert <niljordan23@gmail.com>
2020-09-15 22:41:29 -0500

    [FEATURE] Added TrigramSimmilarity in order to make complex searches

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
-from taggit.models import Tag
 from django.db.models import Count
 from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 from .forms import EmailPostForm, CommentForm, SearchForm
+from .models import Post, Comment
+from taggit.models import Tag
 
 # Create your views here.
 
@@ -101,12 +101,13 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title', 'body')
-            search_query = SearchQuery(query)
+            # search_vector = SearchVector('title', weigh='A') + \
+            #     SearchVector('body', weigh='B')
+            # search_query = SearchQuery(query)
+            
             results = Post.published.annotate(
-                search=search_vector,
-                rank=SearchRank(search_vector, search_query)
-                ).filter(search=search_query).order_by('-rank')
+                similarity=TrigramSimilarity('title', query)
+                ).filter(similarity__gt=0.1).order_by('-similarity')
     return render(request,
                   'blog/post/search.html',
                   {'form': form,
